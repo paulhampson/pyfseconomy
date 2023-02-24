b834113aaa0aa83ee7e630326e583725a5bd4e22
Paul Hampson <528002+paulhampson@users.noreply.github.com>
2023-02-24 21:10:17 +0000

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='pyfseconomy',
-    version='0.2.2',
+    version='0.3.0',
     author="Paul Hampson",
     description='Library for interacting with the FS Economy API',
     long_description=long_description,
